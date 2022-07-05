dc911004011bae469dddd156404807b8001c0cd3
menjingru <62264914+menjingru@users.noreply.github.com>
2022-07-05 14:32:13 +0800

    Update simple.py

--- simple.py
+++ simple.py
@@ -15,17 +15,19 @@ from plan_crop_ import crop_
 # -------------------------------------------------------------------------------------------------------
 
 # 1.安装（省略）
+print('1 upset down!')
 
 
 # 2.构建文件夹结构（已经在global完成）
+print('2 struct down!')
 
 
 # 3.构建标准数据集结构（项目级）    # /home/deepliver2/Disksdd/menjingru/nnUNet_script/nnUNet_dataset_setting.py
 to_setting = False
 
 # （修改区）-----------
-project_id = 1
-project_name = 'spleen'
+project_id = 10
+project_name = 'VESSEL'
 # （修改区停）----------
 
 # dataset.json配置
@@ -35,28 +37,29 @@ dic['name'] = str("Task%02d_%s" % (project_id, project_name))
 # （修改区）-----------
 dic['description'] = 'Segmentation'
 dic['tensorImageSize'] = '3D'
-dic['reference'] = '可参考数据集/home/deepliver2/Disksdd/spleen_5mm'  # 参考
-dic['licence'] = '20220616'
+dic['reference'] = '可参考数据集%s'%source_path  # 参考
+dic['licence'] = '20220704'
 dic['release'] = '0.0'  # 不知道是什么但不重要
 dic['modality'] = {'0': 'CT', }
-dic['labels'] = {'0': 'background', '1': 'spleen', }  # 脾脏
+dic['labels'] = {'0': 'background', '1': 'VESSEL', }  # 血管
 # （修改区停）-----------
 
 if to_setting:
     save_00 = setting(nnUNet_raw_data,source_path,dic,project_id,project_name)
 else:
     # 如果不需要新建项目数据集，可以从已有数据集中手动指定
-    save_00 = '/home/deepliver2/Disksdd/menjingru/dataset/nnUNet_raw_data_base/nnUNet_raw_data/Task05_Try'  # （例）
-
+    save_00 = r'E:\dataset\nnUNet_raw_data_base\nnUNet_raw_data\Task10_VESSEL'  # （例）
+print('3 setting down!')
 
 # 4.转换为官方格式（图加_0000，统一为三维）   # nnUNet_convert_decathlon_task -i /home/deepliver2/Disksdd/menjingru/dataset/nnUNet_raw_data_base/nnUNet_raw_data/Task01_Try
-to_convert = False
+to_convert = True
 if to_convert:
+    print(save_00)
     id_int = convert_(save_00)
 else:
     # 如果不需要转换，可以从已转换数据集中手动指定
     id_int = 1  # （例）
-
+print('4 setting down!')
 
 # 5.预处理     # nnUNet_plan_and_preprocess -t NUM
 #     crop_(id_int=str(id_int))
@@ -68,5 +71,7 @@ else:
 
 
 # 8.测试   # nnUNet_predict -i /home/deepliver2/Disksdd/menjingru/dataset/nnUNet_raw_data_base/nnUNet_raw_data/Task001_Try/imagesTs -o /home/deepliver2/Disksdd/menjingru/dataset/nnUNet_raw_data_base/nnUNet_raw_data/Task001_Try/infersTs -t 1 -m 3d_fullres -f 4
+
+
 
 
